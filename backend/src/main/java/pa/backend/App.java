44180d8d6c93733714f2d18600daa476e67c966c
Prateek <prateek.agarwal.30@gmail.com>
2018-01-20 22:55:58 +0530

    Project Setup :D

---
+++ backend/src/main/java/pa/backend/App.java
@@ -0,0 +1,13 @@
+package pa.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
